83b724908fff27fabec24651e5cf12aea2095195
Giovanni Espina <giovanni.espina@g.msuiit.edu.ph>
2017-06-23 23:18:43 +0800

    added Get Gadgets in Admin function in database

--- db/GadgetRent.sql
+++ db/GadgetRent.sql
@@ -183,3 +183,9 @@ $$
 		from Gadget CROSS JOIN UserAccount where UserAccount.user_id = p_user_id;
 $$
 	language 'sql';
+
+	create or replace function get_gadgetsinadmin(out text, out text, out text, out text, out numeric, out text, out int, out text, out text, out text, out text, out text) returns setof record as
+$$
+	select gadget_item_id, gadget_color, gadget_brandname, gadget_model, gadget_rental_rate, gadget_image, gadget_owner_id, gadget_category_name, gadget_scale, gadget_ram, gadget_memory, gadget_description from Gadget
+$$
+	language 'sql';
