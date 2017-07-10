caa860d2f1122c286bb19a2fb4e06c6c8c3c66f8
Gfreshstyle <giovanni.espina@g.msuiit.edu.ph>
2017-07-10 20:04:22 +0800

    Edited new_user function in functions.sql

--- db/functions.sql
+++ db/functions.sql
@@ -31,27 +31,34 @@ create or replace function new_user(p_first_name varchar, p_middle_name varchar,
 	returns text as
 	$$
 		declare
+			loc_email text;
 			loc_name text;
 			loc_response text;
 		begin
-			select into loc_name first_name || middle_name || last_name from UserAccount where  first_name = p_first_name and middle_name =  p_middle_name and last_name = p_last_name;
-				if loc_name isnull then
-					if p_first_name = '' or p_last_name = '' or p_email = '' or p_password = '' or p_address = '' or p_mobile_no = null then 
-						loc_response = 'Please fill the require field/s';
-					else
-						insert into UserAccount(first_name, middle_name, last_name, email, password, address, mobile_no, role_id)
-							values(p_first_name, p_middle_name, p_last_name, p_email, p_password, p_address, p_mobile_no, p_role_id);
-						
-						loc_response = 'Ok';
-					end if;
+			select into loc_email email from UserAccount where email = p_email;
+				if loc_email isnull then
+					select into loc_name first_name || middle_name || last_name from UserAccount where  first_name = p_first_name and middle_name =  p_middle_name and last_name = p_last_name;
+						if loc_name isnull then
+							if p_first_name = '' or p_last_name = '' or p_email = '' or p_password = '' or p_address = '' or p_mobile_no = null then 
+								loc_response = 'Please fill the require field/s';
+							else
+								insert into UserAccount(first_name, middle_name, last_name, email, password, address, mobile_no, role_id)
+									values(p_first_name, p_middle_name, p_last_name, p_email, p_password, p_address, p_mobile_no, p_role_id);
+								
+								loc_response = 'Ok';
+							end if;
+						else
+							loc_response = 'User already exists';
+						end if;
 				else
-					loc_response = 'User already exists';
-				end if;
+					loc_response = 'Email already exists';
+
+				end if;		
 				return loc_response;
+
 		end; 
 	$$
 		language 'plpgsql';
-
 
 
 
