4e8ed96653a0180c566519c2852d476d02417a83
Gfreshstyle <giovanni.espina@g.msuiit.edu.ph>
2017-07-16 00:48:26 +0800

    Added login function in login.js and edited dashboard

---
+++ app/static/js/login.js
@@ -0,0 +1,74 @@
+auth_user = "";
+
+$(document).ready(function(){
+
+
+});
+
+var auth_user = "";
+var user_role;
+var timer = 0;
+
+$(document).ready(function(){
+
+	// decryptCookie();
+
+});
+
+
+function login(){
+	var email_address = $('#login_email').val();
+	var password = $('#login_password').val();
+
+	var data = JSON.stringify({'email_address':email_address, 'password':password});
+
+	$.ajax({
+
+		type:"POST",
+		url:"http://localhost:5000/login/",
+		contentType:"application/json; charset=utf-8",
+		data:data,
+		dataType:"json",
+
+		success: function(res){
+			
+			if(res.status == 'Ok'){
+
+
+				// for system admin
+				if(res.entries[0].role_id == 1){
+
+
+					
+					user_role = res.entries[0].role_id;
+				}
+
+
+				// for customer
+				if(res.entries[0].role_id == 2){
+
+
+					user_role = res.entries[0].role_id;		
+
+					$('#header-options2').show();
+					$('#header-options1').hide();
+
+				}
+
+
+				alert("You successfully logged in, " + res.entries[0].fname);
+			}
+
+			if(res.status == 'Error'){
+				$('#login-alert').html(
+					'<div class="alert alert-danger"><strong>'+ res.message +'</div>');
+			}
+
+		},
+
+		error: function(e){
+				alert(e);
+		}
+
+	});
+}
